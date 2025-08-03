de483e79f904b2cddb97c9345e63ecdf34271916
水野敬太 <k_mizuno@atsu-maru.co.jp>
2025-08-03 10:46:34 +0900

    fix:ディレクトリ構造の変更

---
+++ src/components/Login/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styles from "./style.module.scss"
+import Link from "next/link"
+import AudiotrackIcon from '@mui/icons-material/Audiotrack';
+
+const Login = () => {
+  return (
+    <div className={styles.form}>
+        <h3 className={styles.form__title}>ログイン</h3>
+
+  <div className={styles.form__item}>
+        <label htmlFor="">メールアドレス</label>
+        <input type="text" placeholder="メールアドレスを入力してください" />
+      </div>
+
+      <div className={styles.form__item}>
+        <label htmlFor="">パスワード</label>
+        <input type="text" placeholder="パスワードを入力してください" />
+      </div>
+    <button className={styles.form__btn}>
+        <AudiotrackIcon color='primary'/>
+        ログイン
+    </button>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from "@/components/Login"
+import Header from '@/components/Header'
+
+const login = () => {
+  return (
+    <div>
+        <Header />
+        <Login />
+    </div>
+  )
+}
+
+export default login
